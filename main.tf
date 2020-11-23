2f1fc3433509111ad864cc1c54fc8f72bda47d00
Jay <javier.arranz@tadaima.studio>
2020-11-23 12:44:06 +0100

    fix tf0.11

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 resource "aws_ses_domain_identity" "ses_domain" {
-  domain = "${var.sesDomain}"
+  domain = var.sesDomain
 }
 
 resource "aws_ses_domain_dkim" "dkim_domain" {
-  domain = "${aws_ses_domain_identity.ses_domain.domain}"
+  domain = aws_ses_domain_identity.ses_domain.domain
 }
 
 resource "aws_route53_record" "ses_verification_record" {
-  zone_id = "${var.zone_id}"
+  zone_id = var.zone_id
   name    = "_amazonses.${aws_ses_domain_identity.ses_domain.domain}"
   type    = "TXT"
   ttl     = "600"
@@ -16,7 +16,7 @@ resource "aws_route53_record" "ses_verification_record" {
 
 resource "aws_route53_record" "dkim_verification_record" {
   count   = 3
-  zone_id = "${var.zone_id}"
+  zone_id = var.zone_id
   name    = "${element(aws_ses_domain_dkim.dkim_domain.dkim_tokens, count.index)}._domainkey.${aws_ses_domain_identity.ses_domain.domain}"
   type    = "CNAME"
   ttl     = "600"
